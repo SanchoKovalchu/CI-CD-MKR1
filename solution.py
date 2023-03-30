d7d161cbc8d98e9145dfe84f482a3f8d249e18d1
okovwork <okovwork@gmail.com>
2023-03-30 09:40:46 +0300

    Getting array of all words

--- solution.py
+++ solution.py
@@ -1,10 +1,25 @@
+import re
 def read_file(file_name):
     # Open the input file
     with open(file_name, 'r') as f:
         # Read the contents of the file
-        contents = f.read()
+        contents = clean_text(f.readlines())
     return contents
+
+def clean_text(text: list[str]) -> list[str]:
+    return [line.rstrip('\n') for line in text]
+
+def split_into_words(contents):
+    words = []
+    for line in contents:
+        line_words = re.split(r'[-+#, .!?():;/]+', line)
+        line_words.remove('')
+        for word in line_words:
+            words.append(word)
+    return words
 
 if __name__ == "__main__":
     contents = read_file("input.txt")
     print(contents)
+    words = split_into_words(contents)
+    print(words)
